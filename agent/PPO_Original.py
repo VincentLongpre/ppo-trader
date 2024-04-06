e37ab06a516bfa8d511856347a414428761c9485
Vincent Longpre <vincent.longpre1729@gmail.com>
2024-04-06 17:31:08 -0400

    added run function to original ppo

--- agent/PPO_Original.py
+++ agent/PPO_Original.py
@@ -97,3 +97,114 @@ class PPO:
             self.critic_optim.zero_grad()
             critic_loss.backward()
             self.critic_optim.step()
+
+class FeedForwardNN(nn.Module):
+    """
+    A standard in_dim-64-64-out_dim Feed Forward Neural Network.
+    """
+    def __init__(self, in_dim, out_dim):
+        """
+        Initialize the network and set up the layers.
+
+        Parameters:
+            in_dim - input dimensions as an int
+            out_dim - output dimensions as an int
+
+        Return:
+            None
+        """
+        super(FeedForwardNN, self).__init__()
+
+        self.layer1 = nn.Linear(in_dim, 64)
+        self.ln1 = nn.LayerNorm(64)
+        self.layer2 = nn.Linear(64, 64)
+        self.layer3 = nn.Linear(64, out_dim)
+
+        nn.init.normal_(self.layer1.weight, mean=0.0, std=0.1)
+        nn.init.normal_(self.layer2.weight, mean=0.0, std=0.1)
+        nn.init.normal_(self.layer3.weight, mean=0.0, std=0.1)
+
+    def forward(self, obs):
+        """
+            Runs a forward pass on the neural network.
+
+            Parameters:
+                obs - observation to pass as input
+
+            Return:
+                output - the output of our forward pass
+        """
+        # Convert observation to tensor if it's a numpy array
+        if isinstance(obs, np.ndarray):
+            obs = torch.tensor(obs, dtype=torch.float)
+
+        activation1 = F.tanh(self.ln1(self.layer1(obs)))
+        activation2 = F.tanh(self.layer2(activation1))
+        output = self.layer3(activation2)
+
+        return output
+    
+# function that runs each episode
+def episode(agent, n_episodes, max_iter = 1000, end_update=True):
+    batch_r, batch_s, batch_a = [], [], []
+
+    r_eps = []
+
+    for _ in range(n_episodes):
+
+        s, _ = agent.env.reset()
+
+        termination, truncation = False, False
+
+        a, _ = agent.select_action(torch.tensor(s, dtype=torch.float))
+
+        r_ep = 0
+
+        t = 0
+
+        # while not (termination or truncation):
+        for _ in range(max_iter):
+            s_prime, r, termination, _, _ = agent.env.step(a)
+
+            a_prime, _ = agent.select_action(torch.tensor(s_prime, dtype=torch.float))
+
+            batch_r.append(r)
+            batch_s.append(s)
+            batch_a.append(a)
+
+            if not end_update:
+                agent.update(s, a , r, s_prime)
+
+            s, a = s_prime, a_prime
+
+            r_ep += r
+
+            t += 1
+
+            if termination:
+                break
+
+        r_eps.append(r_ep)
+
+    batch_r, batch_s, batch_a = torch.tensor(batch_r, dtype=torch.float), torch.tensor(batch_s, dtype=torch.float), torch.tensor(batch_a, dtype=torch.float)
+    if end_update:
+        agent.update(batch_r, batch_s, batch_a)
+
+    return r_eps
+    
+# function that runs each hyperparameter setting
+def hyperparams_run_gradient(agent_class, policy_class, env, learning_rates, gamma, clip, ent_coef, critic_factor, max_grad_norm, n_updates, n_episodes, max_iter):
+    reward_arr_train = np.zeros((len(learning_rates), 50, 1000))
+
+    for i, lr in enumerate(learning_rates):
+        for run in range(10): # 50, 1 is for debugging
+            print(f'lr_{lr}, for run_{run}')
+            agent = agent_class(policy_class, env, lr, gamma, clip, ent_coef, critic_factor, max_grad_norm, n_updates)
+
+            ep_rewards = []
+            for ep in range(100): # 100 is for debugging
+                ep_rewards.extend(episode(agent, n_episodes, max_iter, end_update=True))
+
+            reward_arr_train[i, run, :] = ep_rewards
+
+    return reward_arr_train
